6fcd6f139a5c1b30ef28784fadd4d75f2c92ed37
SLAMHAYA <71561703+SLAMHAYA@users.noreply.github.com>
2022-05-12 22:05:04 +0200

    version12/02/2022

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Carrier;
+use App\Entity\Category;
+use App\Entity\Header;
+use App\Entity\Order;
+use App\Entity\Product;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('La Boutique Marocaine');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Products', 'fa fa-tag', Product::class);
+    }
+}
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Header;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
